ee589854df237b7c72f54dcf2c10b5deaf003199
ateniltonjr <ateniltonjr09@gmail.com>
2025-07-29 01:04:42 -0300

    finalizado!

---
+++ lib2/funcoes_projeto.h
@@ -0,0 +1,264 @@
+#ifndef FUNCOES_PROJETO_H
+#define FUNCOES_PROJETO_H
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "hardware/adc.h"
+#include "hardware/rtc.h"
+#include "pico/stdlib.h"
+
+#include "iniciar_sd.h"
+
+#include "display.h"
+#include "acc_giro.h"
+#include "led_buzzer_buttons.h"
+
+extern char nome_arquivo[32];
+extern int numero_coleta;
+
+extern const int total_amostras;
+extern const int intervalo_ms;
+
+// Função para capturar dados do acelerômetro e giroscópio e salvar em arquivo CSV
+void capture_imu_data_and_save()
+{
+    printf("\nCapturando dados do acelerômetro e giroscópio. Aguarde a finalização...\n");
+    
+    int tempo_total_s = ((total_amostras - 1) * intervalo_ms) / 1000;
+    printf("Tempo estimado: %d segundos\n", tempo_total_s);
+    ssd1306_fill(&ssd, false);
+    escrever(&ssd, "tempo restante", 5, 5, cor);
+    char tempo_str[20];
+    snprintf(tempo_str, sizeof(tempo_str), "%d segundos", tempo_total_s);
+    escrever(&ssd, tempo_str, 5, 20, cor);
+    // Gera nome do arquivo como arquivo1.csv, arquivo2.csv, etc.
+    char nome_arquivo_incrementado[32];
+    snprintf(nome_arquivo_incrementado, sizeof(nome_arquivo_incrementado), "%s%d.csv", nome_arquivo, numero_coleta);
+    numero_coleta++;
+    FIL file;
+    printf("Arquivo de saída: %s\n", nome_arquivo_incrementado);
+    FRESULT res = f_open(&file, nome_arquivo_incrementado, FA_WRITE | FA_CREATE_ALWAYS);
+    if (res != FR_OK)
+    {
+        printf("\n[ERRO] Não foi possível abrir o arquivo para escrita. Monte o cartão.\n");
+        ssd1306_fill(&ssd, false);
+        escrever(&ssd, "ERRO: SD ausente", 5, 5, cor);
+        escrever(&ssd, "Monte o cartão!", 5, 20, cor);
+        return;
+    }
+    UINT bw;
+    int16_t accel[3], gyro[3];
+    absolute_time_t start_time = get_absolute_time();
+    for (int i = 0; i < total_amostras; i++)
+    {
+        ler_acc_giro(accel, gyro); // Função definida em acc_giro.h/lib2
+        char buffer[100];
+        sprintf(buffer, "%d,%d,%d,%d,%d,%d,%d\n", i + 1, accel[0], accel[1], accel[2], gyro[0], gyro[1], gyro[2]);
+        res = f_write(&file, buffer, strlen(buffer), &bw);
+        if (res != FR_OK)
+        {
+            printf("[ERRO] Não foi possível escrever no arquivo. Monte o cartão.\n");
+            ssd1306_fill(&ssd, false);
+            escrever(&ssd, "ERRO ao gravar", 5, 5, cor);
+            escrever(&ssd, "Monte o cartão!", 5, 20, cor);
+            f_close(&file);
+            return;
+        }
+        // Exibe tempo restante a cada 50 amostras e atualiza display
+        if ((i + 1) % 50 == 0 || i == 0) {
+            absolute_time_t now = get_absolute_time();
+            int elapsed_ms = to_ms_since_boot(now) - to_ms_since_boot(start_time);
+            int remaining_ms = (total_amostras - (i + 1)) * intervalo_ms;
+            int remaining_s = remaining_ms / 1000;
+            printf("Amostra %d/%d - Tempo restante estimado: %d segundos\n", i + 1, total_amostras, remaining_s);
+            // Atualiza display
+            ssd1306_fill(&ssd, false);
+            escrever(&ssd, "tempo restante", 5, 5, cor);
+            char tempo_str[20];
+            snprintf(tempo_str, sizeof(tempo_str), "%d segundos", remaining_s);
+            escrever(&ssd, tempo_str, 5, 20, cor);
+        }
+        sleep_ms(intervalo_ms);
+    }
+    f_close(&file);
+    printf("\nDados do acelerômetro e giroscópio salvos no arquivo %s.\n\n", nome_arquivo_incrementado);
+    ssd1306_fill(&ssd, false);
+    escrever(&ssd, "Dados salvos!", 10, 2, cor);
+}
+
+// Função para ler o conteúdo de um arquivo e exibir no terminal
+void read_file(const char *nome_arquivo)
+{
+    FIL file;
+    FRESULT res = f_open(&file, nome_arquivo, FA_READ);
+    if (res != FR_OK)
+    {
+        printf("[ERRO] Não foi possível abrir o arquivo para leitura. Verifique se o cartão está montado ou se o arquivo existe.\n");
+        return;
+    }
+    char buffer[128];
+    UINT br;
+    printf("Conteúdo do arquivo %s:\n", nome_arquivo);
+    while (f_read(&file, buffer, sizeof(buffer) - 1, &br) == FR_OK && br > 0)
+    {
+        buffer[br] = '\0';
+        printf("%s", buffer);
+    }
+    f_close(&file);
+    printf("\nLeitura do arquivo %s concluída.\n\n", nome_arquivo);
+}
+
+static void run_help()
+{
+    printf("\nComandos disponíveis:\n\n");
+    printf("Digite 'a' para montar o cartão SD\n");
+    printf("Digite 'b' para desmontar o cartão SD\n");
+    printf("Digite 'c' para listar arquivos\n");
+    printf("Digite 'd' para mostrar o conteúdo do arquivo\n");
+    printf("Digite 'e' para obter o espaço livre no cartão SD\n");
+    printf("Digite 'f' para capturar dados do ADC e salvar no arquivo\n");
+    printf("Digite 'g' para formatar o cartão SD\n");
+    printf("Digite 'h' para exibir os comandos disponíveis\n");
+    printf("\nEscolha o comando:  ");
+}
+
+typedef void (*p_fn_t)();
+typedef struct
+{
+    char const *const command;
+    p_fn_t const function;
+    char const *const help;
+} cmd_def_t;
+
+static cmd_def_t cmds[] = {
+    {"setrtc", run_setrtc, "setrtc <DD> <MM> <YY> <hh> <mm> <ss>: Set Real Time Clock"},
+    {"format", run_format, "format [<drive#:>]: Formata o cartão SD"},
+    {"mount", run_mount, "mount [<drive#:>]: Monta o cartão SD"},
+    {"unmount", run_unmount, "unmount <drive#:>: Desmonta o cartão SD"},
+    {"getfree", run_getfree, "getfree [<drive#:>]: Espaço livre"},
+    {"ls", run_ls, "ls: Lista arquivos"},
+    {"cat", run_cat, "cat <filename>: Mostra conteúdo do arquivo"},
+    {"help", run_help, "help: Mostra comandos disponíveis"}};
+
+static void process_stdio(int cRxedChar)
+{
+    static char cmd[256];
+    static size_t ix;
+
+    if (!isprint(cRxedChar) && !isspace(cRxedChar) && '\r' != cRxedChar &&
+        '\b' != cRxedChar && cRxedChar != (char)127)
+        return;
+    printf("%c", cRxedChar); // eco
+    stdio_flush();
+    if (cRxedChar == '\r')
+    {
+        printf("%c", '\n');
+        stdio_flush();
+
+        if (!strnlen(cmd, sizeof cmd))
+        {
+            printf("> ");
+            stdio_flush();
+            return;
+        }
+        char *cmdn = strtok(cmd, " ");
+        if (cmdn)
+        {
+            size_t i;
+            for (i = 0; i < count_of(cmds); ++i)
+            {
+                if (0 == strcmp(cmds[i].command, cmdn))
+                {
+                    (*cmds[i].function)();
+                    break;
+                }
+            }
+            if (count_of(cmds) == i)
+                printf("Comando \"%s\" não encontrado\n", cmdn);
+        }
+        ix = 0;
+        memset(cmd, 0, sizeof cmd);
+        printf("\n> ");
+        stdio_flush();
+    }
+    else
+    {
+        if (cRxedChar == '\b' || cRxedChar == (char)127)
+        {
+            if (ix > 0)
+            {
+                ix--;
+                cmd[ix] = '\0';
+            }
+        }
+        else
+        {
+            if (ix < sizeof cmd - 1)
+            {
+                cmd[ix] = cRxedChar;
+                ix++;
+            }
+        }
+    }
+}
+
+#define debounce_delay 300
+volatile uint tempo_interrupcao = 0;
+
+// Variáveis de estado para captura e montagem do SD
+volatile bool captura_ativa = false;
+volatile bool sd_montado = false;
+volatile bool mount_request_flag = false;
+volatile bool capture_request_flag = false;
+
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    uint tempo_atual = to_ms_since_boot(get_absolute_time());
+
+    // Corrige lógica: só executa ação se passou o tempo de debounce
+    if (tempo_atual - tempo_interrupcao > debounce_delay) {
+        if (gpio == botaoA) {
+            // Alterna captura de dados
+            if (!captura_ativa) {
+                printf("Botão A: Solicitando captura de dados...\n");
+                ssd1306_fill(&ssd, false);
+                escrever(&ssd, "Capturando...", 10, 2, cor);
+                capture_request_flag = true;
+            } else {
+                // Não há mecanismo de parada imediata, apenas exibe mensagem
+                printf("Botão A: Parar captura (não implementado interrupção imediata).\n");
+                ssd1306_fill(&ssd, false);
+                escrever(&ssd, "Parar: aguarde", 10, 2, cor);
+            }
+        } else if (gpio == botaoB) {
+            // Alterna montagem/desmontagem do SD
+            if (!sd_montado) {
+                printf("Botão B: Solicitando montagem do SD...\n");
+                ssd1306_fill(&ssd, false);
+                escrever(&ssd, "Montando SD...", 10, 2, cor);
+                mount_request_flag = true;
+            } else {
+                printf("Botão B: Desmontando SD...\n");
+                ssd1306_fill(&ssd, false);
+                escrever(&ssd, "Desmontando SD...", 10, 2, cor);
+                run_unmount_default();
+                sd_montado = false;
+                ssd1306_fill(&ssd, false);
+                escrever(&ssd, "SD Desmontado", 10, 2, cor);
+            }
+        } else if (gpio == botaoJ) {
+            printf("Botão J pressionado. Reiniciando o dispositivo...\n");
+            ssd1306_fill(&ssd, false);
+            escrever(&ssd, "Reiniciando...", 10, 2, cor);
+            reset_usb_boot(0, 0);
+        }
+        tempo_interrupcao = tempo_atual; // Atualiza o tempo da última interrupção
+    }
+}
+
+#endif
---
+++ lib2/iniciar_sd.h
@@ -0,0 +1,294 @@
+#ifndef INICIAR_SD_H
+#define INICIAR_SD_H
+
+#include "ff.h"
+#include "diskio.h"
+#include "f_util.h"
+#include "hw_config.h"
+#include "my_debug.h"
+#include "rtc.h"
+#include "sd_card.h"
+
+static sd_card_t *sd_get_by_name(const char *const name)
+{
+    for (size_t i = 0; i < sd_get_num(); ++i)
+        if (0 == strcmp(sd_get_by_num(i)->pcName, name))
+            return sd_get_by_num(i);
+    DBG_PRINTF("%s: unknown name %s\n", __func__, name);
+    return NULL;
+}
+static FATFS *sd_get_fs_by_name(const char *name)
+{
+    for (size_t i = 0; i < sd_get_num(); ++i)
+        if (0 == strcmp(sd_get_by_num(i)->pcName, name))
+            return &sd_get_by_num(i)->fatfs;
+    DBG_PRINTF("%s: unknown name %s\n", __func__, name);
+    return NULL;
+}
+
+static void run_setrtc()
+{
+    const char *dateStr = strtok(NULL, " ");
+    if (!dateStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int date = atoi(dateStr);
+
+    const char *monthStr = strtok(NULL, " ");
+    if (!monthStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int month = atoi(monthStr);
+
+    const char *yearStr = strtok(NULL, " ");
+    if (!yearStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int year = atoi(yearStr) + 2000;
+
+    const char *hourStr = strtok(NULL, " ");
+    if (!hourStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int hour = atoi(hourStr);
+
+    const char *minStr = strtok(NULL, " ");
+    if (!minStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int min = atoi(minStr);
+
+    const char *secStr = strtok(NULL, " ");
+    if (!secStr)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    int sec = atoi(secStr);
+
+    datetime_t t = {
+        .year = (int16_t)year,
+        .month = (int8_t)month,
+        .day = (int8_t)date,
+        .dotw = 0, // 0 is Sunday
+        .hour = (int8_t)hour,
+        .min = (int8_t)min,
+        .sec = (int8_t)sec};
+    rtc_set_datetime(&t);
+}
+
+static void run_format()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    /* Format the drive with default parameters */
+    FRESULT fr = f_mkfs(arg1, 0, 0, FF_MAX_SS * 2);
+    if (FR_OK != fr)
+        printf("Erro em f_mkfs: %s (%d)\n", FRESULT_str(fr), fr);
+}
+void run_mount()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_mount(p_fs, arg1, 1);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_mount: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = true;
+    printf("Processo de montagem do SD (%s) concluído\n", pSD->pcName);
+}
+
+// Função para montar SD sem depender de argumentos de linha de comando
+void run_mount_default()
+{
+    const char *arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_mount(p_fs, arg1, 1);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_mount: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = true;
+    printf("Processo de montagem do SD (%s) concluído\n", pSD->pcName);
+}
+static void run_unmount()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_unmount(arg1);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_unmount: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = false;
+    pSD->m_Status |= STA_NOINIT; // in case medium is removed
+    printf("SD (%s) desmontado\n", pSD->pcName);
+}
+
+// Função para desmontar SD sem depender de argumentos de linha de comando
+void run_unmount_default()
+{
+    const char *arg1 = sd_get_by_num(0)->pcName;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_unmount(arg1);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_unmount: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    sd_card_t *pSD = sd_get_by_name(arg1);
+    myASSERT(pSD);
+    pSD->mounted = false;
+    pSD->m_Status |= STA_NOINIT;
+    printf("SD (%s) desmontado\n", pSD->pcName);
+}
+static void run_getfree()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = sd_get_by_num(0)->pcName;
+    DWORD fre_clust, fre_sect, tot_sect;
+    FATFS *p_fs = sd_get_fs_by_name(arg1);
+    if (!p_fs)
+    {
+        printf("Número de unidade lógica desconhecido: \"%s\"\n", arg1);
+        return;
+    }
+    FRESULT fr = f_getfree(arg1, &fre_clust, &p_fs);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_getfree: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    tot_sect = (p_fs->n_fatent - 2) * p_fs->csize;
+    fre_sect = fre_clust * p_fs->csize;
+    printf("%10lu KiB de espaço total.\n%10lu KiB disponíveis.\n", tot_sect / 2, fre_sect / 2);
+}
+static void run_ls()
+{
+    const char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+        arg1 = "";
+    char cwdbuf[FF_LFN_BUF] = {0};
+    FRESULT fr;
+    char const *p_dir;
+    if (arg1[0])
+    {
+        p_dir = arg1;
+    }
+    else
+    {
+        fr = f_getcwd(cwdbuf, sizeof cwdbuf);
+        if (FR_OK != fr)
+        {
+            printf("f_getcwd error: %s (%d)\n", FRESULT_str(fr), fr);
+            return;
+        }
+        p_dir = cwdbuf;
+    }
+    printf("Listagem do diretório: %s\n", p_dir);
+    DIR dj;
+    FILINFO fno;
+    memset(&dj, 0, sizeof dj);
+    memset(&fno, 0, sizeof fno);
+    fr = f_findfirst(&dj, &fno, p_dir, "*");
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_findfirst: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    while (fr == FR_OK && fno.fname[0])
+    {
+        const char *pcWritableFile = "arquivo gravável",
+                   *pcReadOnlyFile = "arquivo somente leitura",
+                   *pcDirectory = "diretório";
+        const char *pcAttrib;
+        if (fno.fattrib & AM_DIR)
+            pcAttrib = pcDirectory;
+        else if (fno.fattrib & AM_RDO)
+            pcAttrib = pcReadOnlyFile;
+        else
+            pcAttrib = pcWritableFile;
+        printf("%s [%s] [tamanho=%llu]\n", fno.fname, pcAttrib, fno.fsize);
+
+        fr = f_findnext(&dj, &fno);
+    }
+    f_closedir(&dj);
+}
+static void run_cat()
+{
+    char *arg1 = strtok(NULL, " ");
+    if (!arg1)
+    {
+        printf("Argumento faltando\n");
+        return;
+    }
+    FIL fil;
+    FRESULT fr = f_open(&fil, arg1, FA_READ);
+    if (FR_OK != fr)
+    {
+        printf("Erro em f_open: %s (%d)\n", FRESULT_str(fr), fr);
+        return;
+    }
+    char buf[256];
+    while (f_gets(buf, sizeof buf, &fil))
+    {
+        printf("%s", buf);
+    }
+    fr = f_close(&fil);
+    if (FR_OK != fr)
+        printf("Erro em f_open: %s (%d)\n", FRESULT_str(fr), fr);
+}
+
+#endif
